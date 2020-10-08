ec2ef049a68fd3224d95812bef16a64e681a8e80
LearningStudio1 <70543684+LearningStudio1@users.noreply.github.com>
2020-10-08 09:03:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const axios = require("axios")
-const urls = ["https://respected-sand-desk.glitch.me"]
+const urls = ["https://respected-sand-desk.glitch.me/"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
         })
     }, 60 * 1000);
+
